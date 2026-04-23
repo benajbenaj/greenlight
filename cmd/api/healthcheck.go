079a8282e22e25fe5f224609de587c3f75e9e2f6
benajbenaj <h.beni.benedek@gmail.com>
2026-04-23 09:02:26 +0200

    JSON - 1 - custom

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,14 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 // Declare a handler which writes a plain-text response with information about the
 // application status, operating environment and version.
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s\n", version)
+
+	// Create a map which holds the information that we want to send in the response.
+	env := envelope{
+		"status": "available",
+		"system_info": map[string]string{
+			"environment": app.config.env,
+			"version":     version,
+		},
+	}
+
+	// Pass the map to the json.Marshal() function. This returns a []byte slice
+	// containing the encoded JSON. If there was an error, we log it and send the client
+	// a generic error message.
+	err := app.writeJSON(w, http.StatusOK, env, nil)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "The server encountered a problem and could not process your request", http.StatusInternalServerError)
+	}
 }
---
+++ internal/data/movies.go
@@ -0,0 +1,17 @@
+package data
+
+import (
+	"time"
+)
+
+// Annotate the Movie struct with struct tags to control how the keys appear in the
+// JSON-encoded output.
+type Movie struct {
+	ID        int64     `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	Title     string    `json:"-"`
+	Year      int32     `json:"year,omitempty"`    // Add the omitempty directive
+	Runtime   Runtime   `json:"runtime,omitempty"` // Add the omitempty directive
+	Genres    []string  `json:"genres,omitempty"`  // Add the omitempty directive
+	Version   int32     `json:"version"`
+}
